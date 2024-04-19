900a016d8e3c33d457e5041b7ec342c39a006f4c
denneyl2711 <lukedenneymn@gmail.com>
2024-04-19 10:13:33 -0500

    Added basic functionality to DAWG class.

--- FormalLanguagesFinalProject/Dawg.h
+++ FormalLanguagesFinalProject/Dawg.h
@@ -1,15 +1,16 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 class DawgNode {
 private:
 	vector<DawgNode*> parents;
+	vector<char> letters;
 	vector<DawgNode*> children;
 
 public:
 	DawgNode() {
-
 	}
 
 	void addParent(DawgNode* node) {
@@ -19,4 +20,62 @@ public:
 	void addChild(DawgNode* node) {
 		children.push_back(node);
 	}
+
+	int getNumParents() {
+		return parents.size();
+	}
+
+	int getNumChildren() {
+		return children.size();
+	}
+
+	vector<DawgNode*> getChildren() const {
+		return children;
+	}
+
+	vector<DawgNode*> getParents() const {
+		return parents;
+	}
+
+	vector<char> getLetters()const {
+		return letters;
+	}
+
+	bool hasChild(char letter) {
+		for (DawgNode* child : children) {
+			for (char childLetter : child->getLetters()) {
+				if (childLetter == letter) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+};
+
+class Dawg {
+private:
+	DawgNode* root;
+	vector<string> alphabet;
+
+	Dawg(vector<string> alphabet) {
+		this->alphabet = alphabet;
+		root = new DawgNode();
+	}
+
+	void addWord(string word);
+
+	//simplify the DAWG
+	void reduce();
+
+	void addWord(string word) {
+	}
+
+	//if the user enters "cats" and "cat" is in the DAWG, then "cat" will be returned
+	string findPrefix(string word) {
+
+	}
+
 };
